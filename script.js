be26ed07ba143e067cd7cb67536a5003eada8422
idcso <52316112+idcso@users.noreply.github.com>
2023-06-03 01:44:31 +0600

    Add event listener to the "Grid size" button

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 const container = document.querySelector('.container');
+const setSizeButton = document.querySelector('.button');
 const containerWidth = 480;
 let num = 16;
 
-for (let i = 0; i < num * num; i++) {
-	let div = document.createElement('div');
-	div.style.width = `${containerWidth / num - 2}px`;
-	div.style.height = div.style.width;
-	container.appendChild(div).className = 'squareDiv';
+function createGrid(num) {
+	for (let i = 0; i < num * num; i++) {
+		let div = document.createElement('div');
+		div.style.width = `${containerWidth / num - 2}px`;
+		div.style.height = div.style.width;
+		container.appendChild(div).className = 'squareDiv';
+	}
 }
+
+createGrid(num);
 
 container.addEventListener('mouseover', event => {
 	event.target.style.backgroundColor = 'violet';
 });
+
+setSizeButton.addEventListener('click', () => {
+	for (let i = 0; i < num * num; i++) {
+		let div = document.querySelector('.squareDiv');
+		div.remove();
+	}
+
+	newGrid();
+});
+
+function newGrid() {
+	num = +prompt('Set grid size (0 to 100):');
+
+	if (num > 0 && num < 100) {
+		createGrid(num);
+	} else {
+		alert('Choose a number between 0 and 100');
+		newGrid();
+	}
+}
